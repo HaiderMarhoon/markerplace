7f495303178fc41b2f1b2b195d6f39771c81f247
HaiderMarhoon <haidooer2003@gmail.com>
2025-07-16 15:06:20 +0300

    show listing page

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -1,17 +1,19 @@
 const express = require("express")
 const router = express.Router()
 const Listing = require("../models/listing")
+const isSignedIn = require("../middleware/is-signed-in")
 
 
 
 //new page
-router.get("/new", (req, res) => {
+router.get("/new", isSignedIn, (req, res) => {
     res.render("listings/new.ejs")
 
 })
-
-router.post("/", async (req, res) => {
+// post to dB
+router.post("/", isSignedIn, async (req, res) => {
     try {
+        req.body.seller = req.session.user._id
         await Listing.create(req.body)
         res.redirect("/listings")
     }
@@ -26,6 +28,12 @@ router.get("/",async (req,res) =>{
     const foundListings = await Listing.find()
     console.log(foundListings)
     res.render("listings/index.ejs",{foundListings:foundListings})
+})
+
+//show
+router.get("/:ListingId" , async(req,res) =>{
+    const foundListing = await Listing.findById(req.params.listingId).populate("seller")
+    res.render("listings/show.ejs" , {foundListing:foundListing})
 })
 
 
--- models/listing.js
+++ models/listing.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
 const listingSchema = new mongoose.Schema({
     title: String,
     description: String,
     price: String,
-    image: String
+    image: String,
+    seller:{
+        type: Schema.Types.ObjectId,
+        ref:"User"
+    },
 
 },{
     timestamps: true
