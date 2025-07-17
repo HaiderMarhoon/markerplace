58ba69b0af041c39cb9be12a8d89610f08230a01
HaiderMarhoon <haidooer2003@gmail.com>
2025-07-17 11:24:09 +0300

    edit

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -17,37 +17,64 @@ router.post("/", isSignedIn, async (req, res) => {
         await Listing.create(req.body)
         res.redirect("/listings")
     }
-    catch(error){
+    catch (error) {
         console.log(error)
         res.send("Sometings wrong")
     }
 })
 
 // index page
-router.get("/",async (req,res) =>{
+router.get("/", async (req, res) => {
     const foundListings = await Listing.find()
     console.log(foundListings)
-    res.render("listings/index.ejs",{foundListings:foundListings})
+    res.render("listings/index.ejs", { foundListings: foundListings })
 })
 
 //show
-router.get("/:listingId" , async(req,res) =>{
+router.get("/:listingId", async (req, res) => {
     const foundListings = await Listing.findById(req.params.listingId).populate("seller")
     console.log(foundListings)
-    res.render("listings/show.ejs" , {foundListings:foundListings})
+    res.render("listings/show.ejs", { foundListings: foundListings })
 })
 
-router.delete("/:listingId",async(req,res) =>{
+router.delete("/:listingId", async (req, res) => {
     const foundListings = await Listing.findById(req.params.listingId).populate("seller")
-    if(foundListings.seller._id.equals(req.session.user._id)){
+    if (foundListings.seller._id.equals(req.session.user._id)) {
         await foundListings.deleteOne();
-        res.redirect("/listingsrs")
+        return res.redirect("/listingsrs")
     }
 
     return res.send("Not authorized")
 })
 
+router.get("/:listingId/edit", async (req, res) => {
+    try {
+        const foundListings = await Listing.findById(req.params.listingId).populate("seller")
+        if (foundListings.seller._id.equals(req.session.user._id)) {
+            res.render("listings/edit.ejs", { foundListings: foundListings })
+        }
+    }
+    catch (error) {
+        console.log(error)
+        return res.send("Not authorized")
 
+    }
+})
+
+router.put('/:listingId', async (req, res) => {
+        try {
+        const foundListings = await Listing.findById(req.params.listingId).populate("seller")
+        if (foundListings.seller._id.equals(req.session.user._id)) {
+            await Listing.findByIdAndUpdate(req.params.listingId, req.body, {new: true})
+            return res.redirect(`/listing/${req.params.listingId}`)
+        }
+    }
+    catch (error) {
+        console.log(error)
+        return res.send("Not authorized")
+
+    }
+})
 
 
 
